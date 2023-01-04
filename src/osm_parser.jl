39436c10e38950d410171a8708ce93488245c57f
Marcin <mz.mzurek@gmail.com>
2023-01-04 22:52:32 +0100

    osmfilter linux fix

--- src/osm_parser.jl
+++ src/osm_parser.jl
@@ -86,7 +86,7 @@ function generate_temporary_file(osm_filename::String, poi_type::POITypes.POITyp
         osmfiler_path = joinpath(pathof(OSMgetPOI),"..","..","deps","osmfilter")
         generate_file = pipeline(`$osmfiler_path $input_filepath $osm_query`, stdout = output_filepath)
     elseif Sys.islinux()
-        osmfiler_path = "./"*joinpath(dirname(pathof(OSMgetPOI)),"..","deps","osmfilter")
+        osmfiler_path = joinpath(dirname(pathof(OSMgetPOI)),"..","deps","osmfilter")
         generate_file = pipeline(`$osmfiler_path $input_filepath $osm_query`, stdout = output_filepath)
     end
     process_time = @elapsed run(generate_file) #checking how long it takes to run this function
